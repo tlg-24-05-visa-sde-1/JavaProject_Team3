35fa06fa6d2476a1a5da9ce60b3a8a695ec8cb6c
John-DanielVan <JohnV@tlgcohort.com>
2024-06-17 16:15:10 -0500

    Added functions to Board. Created Token

---
+++ src/Token.java
@@ -0,0 +1,11 @@
+public class Token {
+    private String color;
+
+    public Token(String color) {
+        this.color = color;
+    }
+
+    public String getColor() {
+        return color;
+    }
+}
