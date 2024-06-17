1d10264f0d595d3945112f1d44eb5fa1687754c8
John-DanielVan <JohnV@tlgcohort.com>
2024-06-17 14:30:02 -0500

    Created basic classes

---
+++ src/Board.java
@@ -0,0 +1,7 @@
+public class Board {
+    public void show () {}
+    public boolean dropDisc() {return false;}
+    public boolean checkWin() {return false;}
+    public boolean end() {return false;}
+
+}
