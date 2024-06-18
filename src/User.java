1a4ef82ba702737ea09e3ecd24481364b99372be
Megan Miller <MeganM@tlgcohort.com>
2024-06-18 14:45:08 -0500

    Changed User to an abstract class. Commented & started chooseColumn() method in User. Added constructors to User, Player, BotOpponent

--- src/User.java
+++ src/User.java
@@ -1,2 +1,19 @@
-class User extends Player{
+abstract class User {
+    // protected field(s)/instance variable(s)
+    protected Board board;
+
+    // Constructor initializing User with Board object
+    public User(Board board) {
+        this.board = board;
+    }
+
+    /*
+     * chooseColumn() : 1. Gets a list of available columns.
+     * 2. Based on type of User, the method chooses to either:
+     * prompt Player to choose a column, OR, BotOpponent randomly
+     * chooses a column to dropToken() into.
+     */
+    protected int chooseColumn() {
+
+    }
 }
