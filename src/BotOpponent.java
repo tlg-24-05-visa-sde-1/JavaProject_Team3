b5f24e01532e01cf7d896d5f53e45e76b1d03b44
Megan Miller <MeganM@tlgcohort.com>
2024-06-20 10:22:17 -0500

    changed to extends Player rather than User. Moved shared logic into superclass: Player and corrected code in User and BotOpponent to only handle their part

--- src/BotOpponent.java
+++ src/BotOpponent.java
@@ -1,9 +1,32 @@
-class BotOpponent extends User {
+package com.jbm.connect4.model;
 
-    // Constructor initializing BotOpponent with Board object
-    public BotOpponent (Board board) {
+import java.util.ArrayList;
+import java.util.Random;
+
+class BotOpponent extends Player {
+    // Ctor that initializes BotOpponent with a Board object
+    public BotOpponent(Board board) {
         super(board);
     }
 
+    @Override
+    protected int chooseColumn() {
+        // Gets list of open columns using the method from the Player class
+        ArrayList<Integer> openColumns = getOpenColumns();
 
+        // Randomly select an index from the list of open columns
+        return openColumns.get(new Random().nextInt(openColumns.size()));
+    }
+
+    @Override
+    public void dropToken(String color) {
+        // Choose a column to drop the token
+        int column = chooseColumn();
+        // Attempts to place the token in the chosen column
+        boolean success = board.update(column, new Token(color));
+        if (!success) {
+            // If not successful, throw exception
+            throw new IllegalStateException("Failed to drop token in column " + column);
+        }
+    }
 }
